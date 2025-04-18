2338d2f098c307705f92e6b57d78d19707d43da9
kylie <kcave@smith.edu>
2025-04-18 09:27:32 -0400

    location and item work pt 2

--- Item.java
+++ Item.java
@@ -1,13 +1,53 @@
 public class Item {
-    String name; 
-    String description; 
-    Location currentLocation; 
-    boolean inInventory; 
+    protected String name; 
+    protected String description; 
+    protected Location currentLocation; 
+    protected boolean inInventory; 
 
+    /**
+     * The constructor for the Item class
+     * @param n the name of the item
+     * @param d a description of the item
+     * @param cL the items current location
+     * @param iI if the item is currently located in the inventory or not. 
+     */
     Item(String n, String d, Location cL, boolean iI){
         this.name= n; 
         this.description=d; 
         this.currentLocation= cL; 
         this.inInventory= iI; 
     }
+
+    /**
+     * an acessor for the item's name
+     * @return a string of the item's name
+     */
+    public String getName(){
+        return this.name;
+    }
+
+    /**
+     * an acessor method for the item's description
+     * @return the description for the item 
+     */
+    public String getDescription(){
+        return this.description; 
+    }
+
+    /**
+     * an acessor for the item's location
+     * @return the item's location
+     */
+    public Location getLocation(){
+        return this.getLocation(); 
+    }
+
+    /**
+     * an acessor for the item's boolean attribute "inInventory"
+     * @return T/F wether the item is in the player's inventory 
+     */
+    public boolean inInventory(){
+        return this.inInventory; 
+    }
+
 }
--- Location.java
+++ Location.java
@@ -1,23 +1,65 @@
+import java.util.ArrayList; 
 public class Location {
+
     String name; 
     String description; 
     boolean hasItem; 
+    ArrayList<Item> locationItems; 
 
+    /**
+     * A constuctor for a Location
+     * @param n the name of the location
+     * @param d the description of the location
+     * @param hI T/F wether the location contains and item. 
+     */
     Location(String n, String d, boolean hI){
         this.name=n; 
         this.description=d; 
         this.hasItem= hI; 
+        this.locationItems= new ArrayList<Item>(); 
     }
 
+    /**
+     * An acessor for the name of the location
+     * @return a string with the name of the location
+     */
     public String getName(){
         return this.getName(); 
     }
 
+    /**
+     * an acessor for the description of the location
+     * @return a string with the description of the location
+     */
     public String getDescription(){
         return this.getDescription(); 
     }
     
-    public boolean getHasItem(){
-        return this.getHasItem();
+    /**
+     * an acessor method for wether the Location has an item in it or not
+     * @return T/F wether the location contains at least one item. 
+     */
+    public Item getItem(Item i){
+       if(this.locationItems.contains(i)){
+        return i; 
+       }else{
+        throw new RuntimeException("This Item is not present at your current location."); 
+       }
+    }
+
+    /**
+     * This adds an item to the location's array list of items
+     * @param i the item to be added to the location
+     */
+    public void addItem(Item i){
+        this.locationItems.add(i); 
+    }
+
+    /**
+     * This removes an item from the location's array list of items 
+     * @param i the item to be removed from the array list of items
+     */
+    public void removeItem(Item i){
+        this.locationItems.remove(i); 
     }
 }
