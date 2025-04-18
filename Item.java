9f5f52e5ad899c93947962ef83f1e3dfdd3bac40
kylie <kcave@smith.edu>
2025-04-18 07:53:16 -0400

    attributes and acessors for Locationa and Item

--- Item.java
+++ Item.java
@@ -1,3 +1,13 @@
 public class Item {
-    
+    String name; 
+    String description; 
+    Location currentLocation; 
+    boolean inInventory; 
+
+    Item(String n, String d, Location cL, boolean iI){
+        this.name= n; 
+        this.description=d; 
+        this.currentLocation= cL; 
+        this.inInventory= iI; 
+    }
 }
--- Location.java
+++ Location.java
@@ -1,3 +1,23 @@
 public class Location {
+    String name; 
+    String description; 
+    boolean hasItem; 
+
+    Location(String n, String d, boolean hI){
+        this.name=n; 
+        this.description=d; 
+        this.hasItem= hI; 
+    }
+
+    public String getName(){
+        return this.getName(); 
+    }
+
+    public String getDescription(){
+        return this.getDescription(); 
+    }
     
+    public boolean getHasItem(){
+        return this.getHasItem();
+    }
 }
