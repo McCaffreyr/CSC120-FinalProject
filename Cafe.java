629f4e6d25ddceecd901c4ae4dbf35e8c322c1d3
Maggie McCaffrey <rmccaffrey@smith.edu>
2025-04-20 15:41:48 -0400

    condensed locations

--- Cafe.java
+++
@@ -1,8 +0,0 @@
-public class Cafe extends Location{
-
-    Cafe(String n, String d, boolean hI) {
-        super("Cafe", "This is the school's cafeteria. Dozens of long tables sit in organized rows. Near the front of the room is a kitchen. What could be back there?"
-        , true);
-    }
-    
-}
--- Classroom.java
+++
@@ -1,7 +0,0 @@
-public class Classroom extends Location{
-
-    Classroom(String n, String d, boolean hI) {
-        super(n, d, hI);
-    }
-    
-}
