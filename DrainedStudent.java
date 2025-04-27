6f8335cb2bf343b060aedefc39cf81d732544177
Maggie McCaffrey <rmccaffrey@smith.edu>
2025-04-27 15:40:36 -0400

    added desc, person prog, main prog

--- DrainedStudent.java
+++ DrainedStudent.java
@@ -1,7 +1,16 @@
 public class DrainedStudent extends Student{
 
+    /**
+     * A constructor for a drained student.
+     * @param n name of the drained student.
+     * @param d A physical description of the drained student.
+     * @param cL The current Location of the drained student.
+     * @param h The health of the drained student.
+     * @param a the attackability of the drained student.
+     */
     DrainedStudent(String n, String d, Location cL, int h, Boolean a) {
         super(n, d, cL, h, a);
+        a = false;
     }
 
 }
